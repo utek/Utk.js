92c087762b17e8b7e55c93467b3f347fea78e4e5
Łukasz Bołdys <mail@utek.pl>
2015-01-12 07:41:14 +0100

    jshint fixes

--- src/utk.arrayutils.js
+++ src/utk.arrayutils.js
@@ -1,23 +1,27 @@
 var Utk = (function (self) {
+    'use strict';
     return self;
 }(Utk || {}));
 
-Utk.ArrayUtils = (function () {
+Utk.ArrayUtils = function () {
+    'use strict';
     var self = {};
 
     self.diffArray = function (arrayA, arrayB, fn) {
         var max = Math.max(arrayA.length, arrayB.length);
-        res = [];
+        var res = [];
         fn = typeof fn === 'function' ? fn : false;
         for (var i = 0; i < max; i++) {
             var valA = i < arrayA.length ? arrayA[i] : undefined;
             var valB = i < arrayB.length ? arrayB[i] : undefined;
             for (var j = 0; j < max; j++) {
                 valA = valA === undefined || (j < arrayB.length && (fn ? fn(valA,
-                    arrayB[j]) : valA == arrayB[j])) ? undefined : valA;
+                    arrayB[j]) : valA === arrayB[j])) ? undefined : valA;
                 valB = valB === undefined || (j < arrayA.length && (fn ? fn(valB,
-                    arrayA[j]) : valB == arrayA[j])) ? undefined : valB;
-                if (valA === undefined && valB === undefined) break;
+                    arrayA[j]) : valB === arrayA[j])) ? undefined : valB;
+                if (valA === undefined && valB === undefined) {
+                    break;
+                }
             }
             if (valA !== undefined) {
                 res.push(valA);
@@ -40,5 +44,4 @@ Utk.ArrayUtils = (function () {
             array.splice(index, 1);
         }
     };
-
-});
+};
--- src/utk.colorutils.js
+++ src/utk.colorutils.js
@@ -1,14 +1,16 @@
 var Utk = (function (self) {
+    'use strict';
     return self;
 }(Utk || {}));
 
 Utk.ColorUtils = (function () {
+    'use strict';
     var self = {};
 
     self._componentToHex = function (component) {
         component = Math.floor(component);
         var hex = component.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
+        return hex.length === 1 ? '0' + hex : hex;
     };
 
     self.hexToRgb = function (hex) {
@@ -28,7 +30,7 @@ Utk.ColorUtils = (function () {
     };
 
     self.rgbToHex = function (red, green, blue) {
-        return "#" + self._componentToHex(red) + self._componentToHex(
+        return '#' + self._componentToHex(red) + self._componentToHex(
                 green) +
             self._componentToHex(blue);
     };
@@ -39,7 +41,7 @@ Utk.ColorUtils = (function () {
         var blue = Math.floor(Math.random() * 255 + 1);
 
         if (baseColor) {
-            _baseColor = self.hexToRgb(baseColor);
+            var _baseColor = self.hexToRgb(baseColor);
             red = Math.floor((red + _baseColor.red) / 2);
             green = Math.floor((green + _baseColor.green) / 2);
             blue = Math.floor((blue + _baseColor.blue) / 2);
@@ -48,19 +50,19 @@ Utk.ColorUtils = (function () {
     };
 
     self.averageWith = function (color, base) {
-        _color = self.hexToRgb(color);
-        _base = self.hexToRgb(base);
+        var _color = self.hexToRgb(color);
+        var _base = self.hexToRgb(base);
         var red = Math.floor((_color.red + _base.red) / 2);
         var green = Math.floor((_color.green + _base.green) / 2);
         var blue = Math.floor((_color.blue + _base.blue) / 2);
         return self.rgbToHex(red, green, blue);
     };
 
-    self.valueToColor = function (value) {
-        var a = value / 100;
+    self.valueToColor = function (value, maxVal) {
+        var a = value / maxVal;
         a = (a < 0) ? 0 : ((a > 1) ? 1 : a);
-        var h0 = 120;
-        var h1 = 0;
+        var h0 = 0;
+        var h1 = 120;
         var h = (h0) * (1 - a) + (h1) * (a);
         var h2 = h * 3 / 4;
         var k2r = Math.PI / 180;
@@ -69,16 +71,16 @@ Utk.ColorUtils = (function () {
         var g = (h <= 240) ? Math.sin(h2 * k2r) : 0;
         var b = (h >= 120) ? Math.sin((h2 - 90) * k2r) : 0;
         var avg = (r + g + b) / 3;
-        red = ((r - avg) * 1) + avg;
-        green = ((g - avg) * 1) + avg;
-        blue = ((b - avg) * 1) + avg;
-        red = Math.round(red);
-        green = Math.round(green);
-        blue = Math.round(blue);
+        var red = ((r - avg) * 1) + avg;
+        var green = ((g - avg) * 1) + avg;
+        var blue = ((b - avg) * 1) + avg;
+        red = red * 255;
+        green = green * 255;
+        blue = blue * 255;
         return {
-            red: red * 255,
-            green: green * 255,
-            blue: blue * 255
+            red : Math.round(red),
+            green : Math.round(green),
+            blue : Math.round(blue)
         };
     };
 
