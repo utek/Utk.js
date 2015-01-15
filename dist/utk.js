39f611ea53ae0d0ba2da8e355b1ed4c5181cbd50
Łukasz Bołdys <mail@utek.pl>
2015-01-15 10:33:02 +0100

    build command + minification

---
+++ dist/utk.js
@@ -0,0 +1,136 @@
+var Utk = (function (self) {
+    'use strict';
+    return self;
+}(Utk || {}));
+
+Utk.ArrayUtils = function () {
+    'use strict';
+    var self = {};
+
+    self.diffArray = function (arrayA, arrayB, fn) {
+        var max = Math.max(arrayA.length, arrayB.length);
+        var res = [];
+        fn = typeof fn === 'function' ? fn : false;
+        for (var i = 0; i < max; i++) {
+            var valA = i < arrayA.length ? arrayA[i] : undefined;
+            var valB = i < arrayB.length ? arrayB[i] : undefined;
+            for (var j = 0; j < max; j++) {
+                valA = valA === undefined || (j < arrayB.length && (fn ? fn(valA,
+                    arrayB[j]) : valA === arrayB[j])) ? undefined : valA;
+                valB = valB === undefined || (j < arrayA.length && (fn ? fn(valB,
+                    arrayA[j]) : valB === arrayA[j])) ? undefined : valB;
+                if (valA === undefined && valB === undefined) {
+                    break;
+                }
+            }
+            if (valA !== undefined) {
+                res.push(valA);
+            }
+            if (valB !== undefined) {
+                res.push(valB);
+            }
+        }
+        return res;
+    };
+
+    /**
+     * removes element from array in place
+     * @param  {Array} array   Array to remove element from
+     * @param  {object} element element to remove from array
+     */
+    self.removeElement = function (array, element) {
+        var index = array.indexOf(element);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    };
+};
+
+var Utk = (function (self) {
+    'use strict';
+    return self;
+}(Utk || {}));
+
+Utk.ColorUtils = (function () {
+    'use strict';
+    var self = {};
+
+    self._componentToHex = function (component) {
+        component = Math.floor(component);
+        var hex = component.toString(16);
+        return hex.length === 1 ? '0' + hex : hex;
+    };
+
+    self.hexToRgb = function (hex) {
+        var shortRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shortRegex, function (m, red, green,
+            blue) {
+            return red + red + green + green + blue + blue;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+            hex);
+        return result ? {
+            red: parseInt(result[1], 16),
+            green: parseInt(result[2], 16),
+            blue: parseInt(result[3], 16)
+        } : null;
+    };
+
+    self.rgbToHex = function (red, green, blue) {
+        return '#' + self._componentToHex(red) + self._componentToHex(
+                green) +
+            self._componentToHex(blue);
+    };
+
+    self.randomColor = function (baseColor) {
+        var red = Math.floor(Math.random() * 255 + 1);
+        var green = Math.floor(Math.random() * 255 + 1);
+        var blue = Math.floor(Math.random() * 255 + 1);
+
+        if (baseColor) {
+            var _baseColor = self.hexToRgb(baseColor);
+            red = Math.floor((red + _baseColor.red) / 2);
+            green = Math.floor((green + _baseColor.green) / 2);
+            blue = Math.floor((blue + _baseColor.blue) / 2);
+        }
+        return self.rgbToHex(red, green, blue);
+    };
+
+    self.averageWith = function (color, base) {
+        var _color = self.hexToRgb(color);
+        var _base = self.hexToRgb(base);
+        var red = Math.floor((_color.red + _base.red) / 2);
+        var green = Math.floor((_color.green + _base.green) / 2);
+        var blue = Math.floor((_color.blue + _base.blue) / 2);
+        return self.rgbToHex(red, green, blue);
+    };
+
+    self.valueToColor = function (value, maxVal) {
+        var a = value / maxVal;
+        a = (a < 0) ? 0 : ((a > 1) ? 1 : a);
+        var h0 = 0;
+        var h1 = 120;
+        var h = (h0) * (1 - a) + (h1) * (a);
+        var h2 = h * 3 / 4;
+        var k2r = Math.PI / 180;
+        var r = (h <= 120) ? Math.sin((h2 + 90) * k2r) :
+            ((h >= 240) ? Math.sin((h2 - 180) * k2r) : 0);
+        var g = (h <= 240) ? Math.sin(h2 * k2r) : 0;
+        var b = (h >= 120) ? Math.sin((h2 - 90) * k2r) : 0;
+        var avg = (r + g + b) / 3;
+        var red = ((r - avg) * 1) + avg;
+        var green = ((g - avg) * 1) + avg;
+        var blue = ((b - avg) * 1) + avg;
+        red = red * 255;
+        green = green * 255;
+        blue = blue * 255;
+        return {
+            red : Math.round(red),
+            green : Math.round(green),
+            blue : Math.round(blue)
+        };
+    };
+
+    return self;
+}());
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 'use strict';
 var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
+var jasmine = require('gulp-jasmine'); // jshint ignore:line
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('default', function () {
+gulp.task('test', function () {
     return gulp.src('tests/test_*.js')
         .pipe(jasmine());
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {
+            read: false
+        })
+        .pipe(clean());
+});
+
+gulp.task('build', function () {
+    return gulp.src('src/*.js')
+        .pipe(concat('utk.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename('utk.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+
 });
